1fb6808d6c457192c1b5813591673110e33dbaf8
Wisanu Pornjariyatham <107392417+Krustack@users.noreply.github.com>
2024-12-04 12:40:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,7 @@ function calculate(event) {
     const num2 = +form.num2.value;
     const operator = form.operator.value;
     let result = 0;
-    if (operator === '+') {
-        result = num1 + num2;
-    } else if (operator === '-') {
-        result = num1 - num2;
-    } else if (operator === '*') {
-        result = num1 * num2;
-    } else if (operator === '/') {
-        result = num1 / num2;
-    }else{
-        throw new Error('Invalid operator');
-    }
+
     form.result.value = result;
     form.num1.value = '';
     form.num2.value = '';
